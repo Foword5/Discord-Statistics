5f3434fc016354f36f475c4d10993c6701967b1e
Foword <julesd2509@gmail.com>
2023-01-09 20:36:08 -0500

    testing

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from scripts.functions import *
 from scripts.dataCleanUp import *
+from scripts.dataAnalysis import *
 from config import *
 
+import os
+
+
+# the file containing the messages dataframe, in the newData folder
+messageFile = "message.csv"
+
+messagePath = os.path.join(NEWDATAPATH,messageFile)
+if not os.path.exists(messagePath):
+    readMessages(PACKAGEPATH).to_csv(messagePath)
+
+
+print(messagesPerServer(messagePath))
