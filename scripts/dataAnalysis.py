447fb4d308f1f77f339ecd3b3436bc287f4639f5
Foword <julesd2509@gmail.com>
2023-01-17 18:24:12 -0500

    adding documentation

--- scripts/dataAnalysis.py
+++ scripts/dataAnalysis.py
@@ -2,8 +2,16 @@ from scripts.functions import *
 from scripts.dataCleanUp import *
 
 
-
 def messagesPerServer(dataPath):
+    """
+    Get the number of messages sent in every discord server.
+
+    Parameters :
+        dataPath (int) : the path to the file containing all the messages, created by the function in dataCleanUp.py
+    
+    Return :
+        A dataframe containing the list of every discord server and the number of messages sent in it
+    """
     if not os.path.exists(dataPath):
         return None
 
@@ -22,3 +30,31 @@ def messagesPerServer(dataPath):
     )
     
     return messagesPerSever
+
+
+def messagesPerUser(dataPath):
+    """
+    Get the number of messages sent to every user in private conversation.
+
+    Parameters :
+        dataPath (int) : the path to the file containing all the messages, created by the function in dataCleanUp.py
+    
+    Return :
+        A dataframe containing the list of every users to whom a message was sent and the number of messages sent in it
+    """
+    if not os.path.exists(dataPath):
+        return None
+
+    messages = pd.read_csv(dataPath, dtype = str, index_col=0)
+    messages["Timestamp"] = pd.to_datetime(messages["Timestamp"]) # Changing data type from string to timestamp
+    messages["Type"] = messages["Type"].astype(int)
+
+    messagesPerSever = (
+        messages
+        [messages["Recipient"].notna()] # We take only messages comming from private channel
+        .groupby(["Recipient"]) # grouping the data by user
+        .count() # counting the messages
+        .rename(columns = {"ID" : 'Count'}) # renaming the Id column to a more suitable name
+        ["Count"] # keeping the column we want
+        .reset_index()
+    )
