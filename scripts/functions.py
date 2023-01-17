a9113624d256a9e7856b8adc56341850297e9b3a
Foword <julesd2509@gmail.com>
2023-01-17 18:16:56 -0500

    adding get user by id

--- scripts/functions.py
+++ scripts/functions.py
@@ -8,3 +8,14 @@ def getUserId(path):
     
     with open(os.path.join(path,"account","user.json"), 'r') as userInfoFile: # read the json containing the user's info
         return json.load(userInfoFile)["id"]
+
+def getUserInfoById(id,path):
+
+    if(not os.path.exists(path)): # We check if the path to the data exists
+        return None
+
+    with open(os.path.join(path,"account","user.json"), 'r') as userInfoFile: # read the json containing the user's info
+        for friend in json.load(userInfoFile)["relationships"]:
+            if friend["id"] == id:
+                return friend["user"]
+        return id
