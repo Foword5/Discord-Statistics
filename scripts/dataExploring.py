28cdebfea087f5aff02c267a1e153f1f53043e71
Foword <julesd2509@gmail.com>
2023-01-17 18:53:43 -0500

    Doc correction

--- scripts/dataExploring.py
+++ scripts/dataExploring.py
@@ -2,12 +2,12 @@ from scripts.functions import *
 from scripts.dataCleanUp import *
 
 
-def messagesPerServer(dataPath):
+def messagesPerServer(dataPath:str):
     """
     Get the number of messages sent in every discord server.
 
     Parameters :
-        dataPath (int) : the path to the file containing all the messages, created by the function in dataCleanUp.py
+        dataPath (string) : the path to the file containing all the messages, created by the function in dataCleanUp.py
     
     Return :
         A dataframe containing the list of every discord server and the number of messages sent in it
@@ -32,12 +32,12 @@ def messagesPerServer(dataPath):
     return messagesPerSever
 
 
-def messagesPerUser(dataPath):
+def messagesPerUser(dataPath:str):
     """
     Get the number of messages sent to every user in private conversation.
 
     Parameters :
-        dataPath (int) : the path to the file containing all the messages, created by the function in dataCleanUp.py
+        dataPath (string) : the path to the file containing all the messages, created by the function in dataCleanUp.py
     
     Return :
         A dataframe containing the list of every users to whom a message was sent and the number of messages sent in it
@@ -61,13 +61,23 @@ def messagesPerUser(dataPath):
 
     return messagesPerUser
 
-def mostContentSent(dataPath):
+def mostContentSent(dataPath:str):
+    """
+    Count every message's content
+
+    Parameters :
+        dataPath (string) : the path to the file containing all the messages, created by the function in dataCleanUp.py
+    
+    Return :
+        The dataframe contaning the contents and the count of it
+    """
     if not os.path.exists(dataPath):
         return None
 
     messages = pd.read_csv(dataPath, dtype = str, index_col=0)
     messages["Timestamp"] = pd.to_datetime(messages["Timestamp"]) # Changing data type from string to timestamp
     messages["Type"] = messages["Type"].astype(int)
+    messages["Contents"] = messages["Contents"].str.lower()
 
     contentCount = (
         messages
