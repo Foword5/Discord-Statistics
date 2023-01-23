b921ae1819075f03d95f7a86d47c9eb4c4467f65
Foword <julesd2509@gmail.com>
2023-01-22 19:02:55 -0500

    adding weighted median cumputing

--- scripts/functions.py
+++ scripts/functions.py
@@ -1,6 +1,6 @@
 import os
 import json
-from fpdf import FPDF
+import pandas as pd
 from matplotlib import pyplot as plt
 
 def getUserInfo(path:str):
@@ -44,3 +44,22 @@ def getUserInfoById(id:str,path:str):
             if friend["id"] == id:
                 return friend["user"]
         return id
+
+def weighted_median(values:pd.Series,weights:pd.Series):
+    """
+    Compute the weighted median using the values and the weight
+
+    Parameters :
+        values (pandas.Series) the values
+        weights (pandas.Series) the weights
+
+    Return :
+        the weighted median
+    """
+    middleWeight = int(sum(weights)/2)
+    position = 0
+    i = 0
+    while i < len(values) and position < middleWeight :
+        position += weights[i]
+        i += 1
+    return values[i]-0.5
