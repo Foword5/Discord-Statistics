136852b34e5fa8634f991597fc0302046dd36bf0
Foword <julesd2509@gmail.com>
2023-01-22 19:45:48 -0500

    adding FONT

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 PACKAGE_PATH = "package" # the path to the user's data
 NEW_DATA_PATH = "newData" # the path where the new data will be written
 
+FONT = "Arial"
+
 # a list of all the prefixes whose messages strating with them will be ignored in the statistics
 REMOVED_PREFIX = [
     "$"
