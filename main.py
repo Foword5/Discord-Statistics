a91d9c285d72712a4b7bb383497e94b7fe557e60
Foword <julesd2509@gmail.com>
2023-01-23 21:14:46 -0500

    pdf creation

--- main.py
+++ main.py
@@ -1,18 +1,209 @@
-from scripts.dataCleanUp import readMessages
-from scripts.dataExploring import mostWordSent
+from scripts.dataCleanUp import *
+from scripts.dataExploring import *
+from scripts.dataGraph import *
+from scripts.functions import *
 from config import *
 
+from fpdf import FPDF
 import os
 
 
 # the file containing the messages dataframe, in the newData folder
-messageFile = "message.csv"
+messagesFile = "message.csv"
 
-messagePath = os.path.join(NEWDATAPATH,messageFile)
-if not os.path.exists(messagePath):
-    readMessages(PACKAGEPATH).to_csv(messagePath)
+# creating the messages file if it doesn't exist
+messagesPath = os.path.join(NEW_DATA_FOLDER,messagesFile)
+if not os.path.exists(messagesPath):
+    readMessages(PACKAGE_PATH,REMOVED_PREFIX).to_csv(messagesPath)
 
 
-print(
-    mostWordSent(messagePath)
-)
+###########################################################################
+#                                                                         #
+#                             Building the PDF                            #
+#                                                                         #
+###########################################################################
+
+pdf = FPDF(unit="mm",format=(450,360))
+pdf.add_page()
+pdf.set_font(FONT, size = 15)
+pdf.set_fill_color(230,230,230)
+
+#######################################################################
+#                        Messages sent per sever                      #
+#######################################################################
+
+pdf.set_xy(10,10)
+pdf.cell(100,100, fill=True) # we draw the cell
+pdf.set_xy(10,10)
+
+dataMessagePerServer = messagesPerServer(messagesPath).sort_values("Count",ascending=False) # getting the proper data
+plotMessagePerServer = messagesPerServerGraph(dataMessagePerServer) # getting the graph to print
+
+pdf.set_font(FONT, size = 20)
+pdf.cell(100,10,txt="Messages sent per server", align="C", ln=1) # title
+
+imgPath = os.path.join(NEW_DATA_FOLDER,"imageGraph1.png") # we create the path for the new image
+plotMessagePerServer.savefig(imgPath,transparent=True,bbox_inches='tight') # we save the plot as a png file
+
+pdf.image(imgPath,15,20,0,35) # we add the image to the pdf
+
+dataToPrint = dataMessagePerServer[:][0:10].reset_index().drop("index", axis=1) # we prepare the data we're going to print under the pie chart
+printString = ""
+
+for i in range(len(dataToPrint)): # in case there isn't 10 different server we iterate the length of the data
+    printString += str(i+1) + ". " + dataToPrint["GuildName"][i][0:35] + " - " + str(dataToPrint["Count"][i]) + "\n"
+
+printString = printString.encode('latin-1', 'replace').decode('latin-1')
+
+pdf.set_xy(20,55)
+pdf.set_font(FONT, size = 12)
+pdf.multi_cell(90,5,txt=printString, align="L") # we add the text to the pdf
+
+#######################################################################
+#                            Messages size                            #
+#######################################################################
+
+pdf.set_xy(120,10)
+pdf.cell(200,100, fill=True) # we draw the cell
+pdf.set_xy(120,10)
+
+pdf.set_font(FONT, size = 20)
+pdf.cell(200,10,txt="Length of the messages sent", align="C", ln=1) # title
+pdf.set_font(FONT, size = 12)
+
+dataMessageLength = messageSize(messagesPath)
+plotMessageLength = messageSizeGraph(dataMessageLength)
+
+imgPath = os.path.join(NEW_DATA_FOLDER,"imageGraph3.png") # we create the path for the new image
+plotMessageLength.savefig(imgPath,transparent=True,bbox_inches='tight') # we save the plot as a png file
+
+pdf.image(imgPath,120,20,200,0) # we add the image to the pdf
+
+#######################################################################
+#                        Messages sent per user                       #
+#######################################################################
+
+pdf.set_xy(330,10)
+pdf.cell(100,100, fill=True) # we draw the cell
+pdf.set_xy(330,10)
+
+dataMessagePerUser = messagesPerUser(messagesPath, PACKAGE_PATH).sort_values("Count",ascending=False) # getting the proper data
+plotMessagePerUser = messagesPerUserGraph(dataMessagePerUser) # getting the graph to print
+
+pdf.set_font(FONT, size = 20)
+pdf.cell(100,10,txt="Messages sent per user", align="C", ln=1) # we add the title
+
+imgPath = os.path.join(NEW_DATA_FOLDER,"imageGraph2.png") # we create the path for the image
+plotMessagePerUser.savefig(imgPath,transparent=True,bbox_inches='tight') # we save the plot as a png file
+
+pdf.image(imgPath,335,20,0,35) # we add the image to the pdf
+
+dataToPrint = dataMessagePerUser[:][0:10].reset_index().drop("index", axis=1) # we prepare the data we're going to print under the pie chart
+printString = ""
+
+for i in range(len(dataToPrint)): # in case there isn't 10 different server we iterate the length of the data
+    printString += str(i+1) + ". " + dataToPrint["RecipientName"][i][0:35] + " - " + str(dataToPrint["Count"][i]) + "\n"
+
+printString = printString.encode('latin-1', 'replace').decode('latin-1')
+
+pdf.set_xy(340,55)
+pdf.set_font(FONT, size = 12)
+pdf.multi_cell(90,5,txt=printString, align="L") #  we add the text to the pdf
+
+#######################################################################
+#                         Most Sent messages                          #
+#######################################################################
+
+pdf.set_xy(10,120)
+pdf.cell(100,210, fill=True) # we draw the cell# we draw the cell
+pdf.set_xy(10,120)
+
+pdf.set_font(FONT, size = 20)
+pdf.cell(100,10,txt="Most sent messsages", align="C", ln=1) # we add the title
+pdf.set_font(FONT, size = 12)
+
+dataMostSendMessages = mostContentSent(messagesPath).sort_values("Count",ascending=False).reset_index()[:38]
+printString = ""
+
+for i in range(len(dataMostSendMessages)):
+    content = dataMostSendMessages["Contents"][i]
+    if len(content) > 35 :
+        printString += str(i+1) + ". " + content[:32] + "... (" + str(dataMostSendMessages["Count"][i]) + ") \n"
+    else :
+        printString += str(i+1) + ". " + content + " (" + str(dataMostSendMessages["Count"][i]) + ") \n"
+
+printString = printString.encode('latin-1', 'replace').decode('latin-1')
+
+pdf.set_xy(20,130)
+pdf.multi_cell(90,5,txt=printString, align="L") #  we add the text to the pdf
+
+#######################################################################
+#                           Most used word                            #
+#######################################################################
+
+pdf.set_xy(330,120)
+pdf.cell(100,210, fill=True) # we draw the cell
+pdf.set_xy(330,120)
+
+pdf.set_font(FONT, size = 20)
+pdf.cell(100,10,txt="Most used word", align="C", ln=1) # we add the title
+pdf.set_font(FONT, size = 12)
+
+dataMostUsedWord = mostWordSent(messagesPath).sort_values("Count",ascending=False).reset_index()[:38]
+printString = ""
+
+for i in range(len(dataMostUsedWord)):
+    word = dataMostUsedWord["Word"][i]
+    if len(content) > 35 :
+        printString += str(i+1) + ". " + word[:32] + "... (" + str(dataMostUsedWord["Count"][i]) + ") \n"
+    else :
+        printString += str(i+1) + ". " + word + " (" + str(dataMostUsedWord["Count"][i]) + ") \n"
+
+printString = printString.encode('latin-1', 'replace').decode('latin-1')
+
+pdf.set_xy(340,130)
+pdf.multi_cell(90,5,txt=printString, align="L") #  we add the text to the pdf
+
+#######################################################################
+#                          Favorite channel                           #
+#######################################################################
+
+pdf.set_xy(120,120)
+pdf.cell(200,100, fill=True) # we draw the cell
+pdf.set_xy(120,120)
+
+pdf.set_font(FONT, size = 20)
+pdf.cell(200,10,txt="Most popular channels", align="C", ln=1) # we add the title
+pdf.set_font(FONT, size = 12)
+
+dataFavChannel = messagesPerChannel(messagesPath)
+plotFavChannel = messagesPerChannelGraph(dataFavChannel, PACKAGE_PATH)
+
+imgPath = os.path.join(NEW_DATA_FOLDER,"imageGraph4.png") # we create the path for the image
+plotFavChannel.savefig(imgPath,transparent=True,bbox_inches='tight') # we save the plot as a png file
+
+pdf.image(imgPath,120,130,200,0) # we add the image to the pdf
+
+#######################################################################
+#                        Messages sent per day                        #
+#######################################################################
+
+pdf.set_xy(120,230) 
+pdf.cell(200, 100, fill=True) # we draw the cell
+pdf.set_xy(120,230)
+
+pdf.set_font(FONT, size = 20)
+pdf.cell(200,10,txt="Messages per day", align="C", ln=1) # we add the title
+pdf.set_font(FONT, size = 12)
+
+dataMessagesPerDay = messsagePerDay(messagesPath)
+plotMessagesPerDay = messagePerDayGraph(dataMessagesPerDay)
+
+imgPath = os.path.join(NEW_DATA_FOLDER,"imageGraph5.png") # we create the path for the image
+plotMessagesPerDay.savefig(imgPath,transparent=True,bbox_inches='tight') # we save the plot as a png file
+
+pdf.image(imgPath,120,240,200,0) # we add the image to the pdf
+
+
+
+pdf.output("newData/test.pdf")
